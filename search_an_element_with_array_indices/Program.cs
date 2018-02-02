7a4f6dfefee94d6d8f5cec4282063faa772d03b5
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2018-02-03 09:56:55 +1100

    Initial commit

---
+++ search_an_element_with_array_indices/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *  C# Program to Search an element with Array Indices
+ */
+
+using System;
+
+namespace search_an_element_with_array_indices
+{
+    class ArrayBinarySearch
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 0, 10, 100, 1000, 1000000 };
+            Console.WriteLine("Array indices and elements: ");
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.Write("[{0}]={1, -5}", i, ints[i]);
+            }
+            Console.WriteLine();
+            FindObject(ints, 25);
+            FindObject(ints, 1000);
+            FindObject(ints, 2000000);
+            Console.ReadLine();
+        }
+    }
+}
